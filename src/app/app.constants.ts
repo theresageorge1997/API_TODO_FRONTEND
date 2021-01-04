920d0ab7cb9ecef9a27e65022d74b119e9fff513
theresageorge1997 <theresageorgr1997@gmail.com>
2021-01-04 14:53:48 +0530

    updated

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,8 +1,9 @@
-
-export const API_URL = ''
-
+//export const API_URL = 'http://localhost:7071/api/todos';
+//export const API_URL = 'https://ust-todo-app.azurewebsites.net/api/todos';
+export const API_URL =
+  'https://todo-api-ust-management.azure-api.net/ust-todo-app/todos';
 
 export const POOL_DATA = {
-    UserPoolId: 'us-east-1_GMdZm52Wh',//'us-east-1_7dF78dyvw',
-    ClientId: '7h6ivmhtdrfduhu4jjvfjqtduo'//'7pn13e127fqnptfp0glb3bke8e'
+  UserPoolId: 'us-east-1_GMdZm52Wh', //'us-east-1_7dF78dyvw',
+  ClientId: '7h6ivmhtdrfduhu4jjvfjqtduo', //'7pn13e127fqnptfp0glb3bke8e'
 };
